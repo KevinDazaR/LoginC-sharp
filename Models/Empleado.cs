c33ef2669d817448d45a6cc1661960dee2ab475b
KevinDazaR <61398857+KevinDazaR@users.noreply.github.com>
2024-04-15 21:37:27 -0500

    Add files via upload

---
+++ Models/Empleado.cs
@@ -0,0 +1,18 @@
+namespace EmpleadoLogin.Models
+{
+    public class Empleado
+    {
+        public int ? Id { get; set; }
+        public string ? Nombres { get; set; }
+        public string ? Apellidos { get; set; }
+        public string ? Correo { get; set; }
+
+        public string ? Contraseña { get; set; }
+
+        public DateTime ? Hora_Entrada { get; set; }
+
+        public DateTime ? Hora_Salida { get; set; }
+        public string ? Estado { get; set; }
+
+    }
+}
